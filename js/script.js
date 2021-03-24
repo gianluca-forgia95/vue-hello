075afabcfca3b3b69f13ce95a8384f902550eec9
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-03-24 14:58:58 +0100

    aggiungo immagine, lista e orario di caricamento dal data

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 //Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+//Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 var root = new Vue({
 el: '#root',
 data: {
-     
-}
-})
+     text: 'Forza Roma',
+     img: 'img/pantheon.jpg',
+     message: 'Hai caricato questa pagina il ' + new Date().toLocaleString(),
+     list: [
+       'Seguire la lezione',
+       'Leggere la documentazione VueJs',
+       'Fare prove'
+         ],
+    }
+});
